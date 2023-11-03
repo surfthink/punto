f26899a7e79cfa368496d4af2136536fee2954ea
bb-33 <jonolj56@gmail.com>
2023-11-03 13:53:04 +0000

    added concept of decks and hand

--- src/app/_components/Punto.tsx
+++ src/app/_components/Punto.tsx
@@ -2,7 +2,27 @@
 import { useState } from "react";
 import Board from "./Board";
 import Hand from "./Hand";
-import GameLogic from "./GameLogic";
+import GameLogic, { Card, Color } from "./GameLogic";
+
+type Deck = Card[];
+type Decks = {
+  [key in Color]?: Deck;
+};
+
+function initDecks(colors: Color[]) {
+  const decks: Decks = {};
+  console.log(colors);
+  colors.forEach((c) => {
+    const deck: Deck = [];
+    const color = c;
+    for (let value = 1; value < 10; value++) {
+      deck.push({ value, color });
+    }
+    decks[color] = deck;
+  });
+  console.log("init decks", decks);
+  return decks;
+}
 
 /**
  * This component will be the brains of the punto game. It will have the grid display beneath it.
@@ -12,9 +32,26 @@ import GameLogic from "./GameLogic";
  */
 export default function Punto() {
   const [board, setBoard] = useState(GameLogic.newBoard(11));
+  // the top of each deck will be the hand
+  const [decks, setDecks] = useState({ ...initDecks(["blue"]) });
+  const [player, setPlayer] = useState<Color>("blue"); // this will probably need to change into a more complex object
 
   const handlePlacement = (x: number, y: number) => () => {
-    setBoard(GameLogic.place([...board], x, y, "blue", 3));
+    const deck = decks[player];
+
+    console.log("handlePlacement");
+    if (!!deck && deck.length > 0) {
+      const card = deck[0];
+      setBoard([...GameLogic.place([...board], x, y, card.color, card.value)]);
+      drawCard(player);
+    }
+  };
+
+  const drawCard = (color: Color) => {
+    const deck = decks[color];
+    if (!deck) throw new Error("Deck color does not exist");
+    // new deck has everything but the top element
+    setDecks({ ...decks, [color]: deck.slice(1) });
   };
 
   return (
