9e537408737528a561f4b8a659d81b7519110df6
bb-33 <jonolj56@gmail.com>
2023-11-03 12:14:04 +0000

    renamed some types

--- src/app/_components/GameLogic.ts
+++ src/app/_components/GameLogic.ts
@@ -1,14 +1,18 @@
 export type Color = "red" | "yellow" | "green" | "blue";
 
+export interface Card {
+  value: number;
+  color: Color;
+}
 /**
  * holds the game state
  */
 export interface PlaceDetails {
-  state: "open" | "closed" | "filled";
-  value?: number;
-  color?: Color;
+  state: "open" | "closed";
+  card?: Card;
 }
 
+export type Board = PlaceDetails[][];
 function newBoard(size: number) {
   const board: PlaceDetails[][] = [];
   for (let i = 0; i < size; i++) {
@@ -22,16 +26,14 @@ function newBoard(size: number) {
 }
 
 function place(
-  board: PlaceDetails[][],
+  board: Board,
   x: number,
   y: number,
   color: Color,
   value: number
 ) {
   console.log(`card placed at ${x},${y} by ${color}`);
-  board[y][x].state = "filled";
-  board[y][x].value = value;
-  board[y][x].color = color;
+  board[y][x].card = { value, color };
 
   for (let j = y - 1; j < y + 2; j++) {
     for (let i = x - 1; i < x + 2; i++) {
@@ -45,19 +47,19 @@ function place(
   return finalBoard;
 }
 
-function closeInvalidOpenPlaces(board: PlaceDetails[][]) {
+function closeInvalidOpenPlaces(board: Board) {
   const b = [...board];
   //if the width is 6 and there is no placed cards in the row then close it all
   b.forEach((row) => {
-    if (row.find((p) => p.state === "filled")) return;
+    if (row.find((p) => !!p.card)) return;
     if (getHeight(board) >= 6) {
       row.forEach((p) => (p.state = "closed"));
     }
   });
   // now do the same for the columns
-  const bT = transpose(b);
+  const bT = transpose<PlaceDetails>(b);
   bT.forEach((row) => {
-    if (row.find((p) => p.state === "filled")) return;
+    if (row.find((p) => !!p.card)) return;
     if (getWidth(board) >= 6) {
       row.forEach((p) => (p.state = "closed"));
     }
@@ -66,17 +68,17 @@ function closeInvalidOpenPlaces(board: PlaceDetails[][]) {
   return transpose(bT);
 }
 
-function getHighestFilledRow(board: PlaceDetails[][]) {
+function getHighestFilledRow(board: Board) {
   const b = [...board];
-  return b.findIndex((row) => row.findIndex((p) => p.state === "filled") >= 0);
+  return b.findIndex((row) => row.findIndex((p) => !!p.card) >= 0);
 }
 
-function getLeftestFilledCol(board: PlaceDetails[][]) {
+function getLeftestFilledCol(board: Board) {
   const b = transpose([...board]);
-  return b.findIndex((row) => row.findIndex((p) => p.state === "filled") >= 0);
+  return b.findIndex((row) => row.findIndex((p) => !!p.card) >= 0);
 }
 
-function canBeOpened(board: PlaceDetails[][], x: number, y: number) {
+function canBeOpened(board: Board, x: number, y: number) {
   const width = getWidth(board);
   const height = getHeight(board);
   const leftest = getLeftestFilledCol(board);
@@ -104,20 +106,19 @@ function canBeOpened(board: PlaceDetails[][], x: number, y: number) {
   return true;
 }
 
-function transpose(matrix: Array<Array<any>>) {
+function transpose<T>(matrix: T[][]) {
   let [row] = matrix;
   return row.map((value, column) => matrix.map((row) => row[column]));
 }
 
-function getWidth(board: PlaceDetails[][]) {
+function getWidth(board: Board) {
   const b = [...board];
-  return transpose(b).filter((row) => row.find((p) => p.state === "filled"))
-    .length;
+  return transpose(b).filter((row) => row.find((p) => !!p.card)).length;
 }
 
-function getHeight(board: PlaceDetails[][]) {
+function getHeight(board: Board) {
   const b = [...board]; // dont want to ruin the board
-  return b.filter((row) => row.find((p) => p.state === "filled")).length;
+  return b.filter((row) => row.find((p) => !!p.card)).length;
 }
 
 const GameLogic = {
