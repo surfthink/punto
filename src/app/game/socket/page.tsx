713e0caddebd93dfd4bc375f4d29aad971acfd62
bb-33 <jonolj56@gmail.com>
2023-11-09 18:52:01 +0000

    prototyping using sockets to create rooms

--- src/app/game/socket/page.tsx
+++ src/app/game/socket/page.tsx
@@ -1,42 +1,92 @@
 "use client";
-import { useEffect } from "react";
+import React from "react";
+import { useEffect, useState } from "react";
 
 export default function Page() {
+  const [room, setRoom] = useState(null);
+  const roomIdInput = React.createRef<HTMLInputElement>();
+
   useEffect(() => {
     (async () => {
       console.log("sending request to", process.env.NEXT_PUBLIC_BACKEND_URL);
       const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL as string);
       const body = await res.json();
       console.log(body);
-
-      console.log("testing websocket");
-      const ws = new WebSocket(
-        (process.env.NEXT_PUBLIC_WS_URL as string) + "ws",
-        "json"
-      );
-      console.log("**CREATEDWEBSOCKET");
-      ws.onopen = (event) => {
-        console.log("**ONOPEN");
-        ws.send("NEXTjs says hi :)");
-      };
-      ws.onmessage = (event) => {
-        console.log("**ONMESSAGE");
-        const msg = JSON.parse(event.data);
-        console.log(msg);
-      };
     })();
-
-    // const ws = new WebSocket(process.env.BACKEND_URL as string);
-
-    // ws.onopen = () => {
-    //   console.log("**ON OPEN");
-    //   ws.send("This is coming from the client!");
-    // };
   }, []);
 
+  const makeRoom = () => {
+    console.log("Making new room");
+    const ws = new WebSocket(
+      (process.env.NEXT_PUBLIC_WS_URL as string) + "/ws",
+      "json"
+    );
+    console.log("**CREATEDWEBSOCKET");
+    ws.onopen = (event) => {
+      console.log("**ONOPEN");
+    };
+    ws.onmessage = (event) => {
+      console.log("**ONMESSAGE");
+      const body = JSON.parse(event.data);
+      setRoom(body.roomId);
+    };
+  };
+
+  const joinRoom = (roomId: string) => {
+    console.log("joining room");
+
+    const ws = new WebSocket(
+      (process.env.NEXT_PUBLIC_WS_URL as string) + "/join",
+      "json"
+    );
+    console.log("**CREATEDWEBSOCKET");
+
+    ws.onopen = (event) => {
+      console.log("**ONOPEN");
+      ws.send(JSON.stringify({ roomId }));
+    };
+
+    ws.onmessage = (event) => {
+      console.log("**ONMESSAGE");
+      const body = JSON.parse(event.data);
+      if (body.roomId) setRoom(body.roomId);
+    };
+  };
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    if (roomIdInput.current) joinRoom(roomIdInput.current.value);
+  };
+
   return (
-    <>
-      <h1>Testing connection with backend</h1>
-    </>
+    <div className="flex flex-col items-center">
+      <h1 className="text-3xl">Testing room connection with backend</h1>
+      <br></br>
+      {!room && (
+        <>
+          <button className="border" onClick={makeRoom}>
+            Create room
+          </button>
+          <p>or</p>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="text-black"
+              name="roomId"
+              placeholder="enter room id"
+              type="text"
+              ref={roomIdInput}
+            ></input>
+            <button className="border" type="submit">
+              Join Room
+            </button>
+          </form>
+        </>
+      )}
+      {!!room && (
+        <p>
+          Connected to <b>{room}</b>
+        </p>
+      )}
+    </div>
   );
 }
