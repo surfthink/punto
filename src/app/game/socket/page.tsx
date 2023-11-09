5fa4bbb838819b11874c276755bf606df7c14aeb
bb-33 <jonolj56@gmail.com>
2023-11-09 14:59:13 +0000

    connecting to the elysia fly backend. Setting environment vars

---
+++ src/app/game/socket/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { useEffect } from "react";
+
+export default function Page() {
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL as string);
+      const body = await res.json();
+      console.log(body);
+    })();
+
+    // const ws = new WebSocket(process.env.BACKEND_URL as string);
+
+    // ws.onopen = () => {
+    //   console.log("**ON OPEN");
+    //   ws.send("This is coming from the client!");
+    // };
+  }, []);
+
+  return (
+    <>
+      <h1>Testing bun backend</h1>
+    </>
+  );
+}
