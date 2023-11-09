c3f9ae83282857b19fd83dddef29faf8cbda92e7
bb-33 <jonolj56@gmail.com>
2023-11-09 15:30:03 +0000

    testing websockets on deployment

--- src/app/game/socket/page.tsx
+++ src/app/game/socket/page.tsx
@@ -8,7 +8,22 @@ export default function Page() {
       const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL as string);
       const body = await res.json();
       console.log(body);
-      console.log("silly");
+
+      console.log("testing websocket");
+      const ws = new WebSocket(
+        (process.env.NEXT_PUBLIC_WS_URL as string) + "ws",
+        "json"
+      );
+      console.log("**CREATEDWEBSOCKET");
+      ws.onopen = (event) => {
+        console.log("**ONOPEN");
+        ws.send("NEXTjs says hi :)");
+      };
+      ws.onmessage = (event) => {
+        console.log("**ONMESSAGE");
+        const msg = JSON.parse(event.data);
+        console.log(msg);
+      };
     })();
 
     // const ws = new WebSocket(process.env.BACKEND_URL as string);
@@ -21,7 +36,7 @@ export default function Page() {
 
   return (
     <>
-      <h1>Testing bun backend</h1>
+      <h1>Testing connection with backend</h1>
     </>
   );
 }
