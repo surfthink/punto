5c274cb0cb42514aec0a29d18c81b80381d7880a
bb-33 <jonolj56@gmail.com>
2023-11-14 16:47:06 +0000

    checked that start game works... ready now to think about how to control the actual game

--- src/app/_components/RoomLoader.tsx
+++ src/app/_components/RoomLoader.tsx
@@ -1,12 +1,49 @@
+import { useState } from "react";
+import {
+  GeneralPlayerInfo,
+  JoinedEvent,
+  PlayerJoinedEvent,
+  PuntoEvent,
+} from "../_hooks/interfaces";
 import { useRoom } from "../_hooks/useRoom";
 import RoomInfo from "./RoomInfo";
+import { Color } from "./GameLogic";
+import Board from "./Board";
 
 interface RoomLoaderProps {
   room: string;
 }
 
 export default function RoomLoader({ room }: RoomLoaderProps) {
-  const { players, color, socket } = useRoom(room);
+  const [players, setPlayers] = useState<GeneralPlayerInfo[]>();
+  const [color, setColor] = useState<Color>();
+
+  const [playing, setPlaying] = useState(false);
+
+  const handleMessage = (event: MessageEvent) => {
+    console.log("**RECEIVEMESSAGE");
+    const body = JSON.parse(event.data) as PuntoEvent<unknown>;
+    console.log("server message: ", body);
+    switch (body.eventType) {
+      case "JOINED":
+        const e = body as JoinedEvent;
+        setPlayers(e.data.players);
+        setColor(e.data.players.find((p) => p.id === e.data.playerId)?.color);
+        break;
+      case "PLAYER_JOINED":
+        setPlayers((body as PlayerJoinedEvent).data.players);
+        break;
+      case "PLAYER_LEFT":
+        setPlayers((body as PlayerJoinedEvent).data.players);
+        break;
+      case "START_GAME":
+        console.log("GAME STARTED!");
+        setPlaying(true);
+        break;
+    }
+  };
+
+  const { socket } = useRoom(room, handleMessage);
 
   return (
     <>
@@ -16,7 +53,16 @@ export default function RoomLoader({ room }: RoomLoaderProps) {
         playerColor={color}
         roomId={room}
       ></RoomInfo>
-      <button onClick={() => socket?.send("PUSHHHHHH")}>Push message</button>
+      {!playing && (
+        <button
+          onClick={() =>
+            socket?.send(JSON.stringify({ action: "START_GAME", data: {} }))
+          }
+        >
+          Start Game
+        </button>
+      )}
+      {playing && <p>The game has begun!</p>}
     </>
   );
 }
