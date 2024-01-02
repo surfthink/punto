072498b4cf53c910ed417001dcf8c080fc82362b
bb-33 <jonolj56@gmail.com>
2024-01-02 12:59:05 +0800

    deployment bug

--- src/app/api/room/[id]/route.ts
+++ src/app/api/room/[id]/route.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import { joinRoom } from "../room";
-import { authOptions } from "../../auth/[...nextauth]/route";
+import { authOptions } from "../../auth/[...nextauth]/authOptions";
 
 export async function GET(
   request: Request,
--- src/app/api/room/create/route.ts
+++ src/app/api/room/create/route.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]/route";
 import { createRoom, randomUniqueCode } from "../room";
+import { authOptions } from "../../auth/[...nextauth]/authOptions";
 
 export async function GET() {
   const id = await randomUniqueCode(4);
