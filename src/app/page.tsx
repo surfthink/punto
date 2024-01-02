0c870968e3f2c82942601a65f0952b3c0801368b
bb-33 <jonolj56@gmail.com>
2024-01-02 12:47:29 +0800

    fiddled with home message and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,84 @@
+"use client";
 import Link from "next/link";
-import { Authentication } from "./_components/authenticate/Authentication";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, useSession } from "next-auth/react";
+import { LoginOrOut } from "./_components/authenticate/Authentication";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-3xl font-bold underline">Punto Online</h1>
       <p>
-        An online version of the dutch table top game. I was shown it by some
-        friends while traveling and decided it would be the perfect project to
-        explore using web sockets with Next JS. Check out the actual{" "}
+        An online version of the European table top game. I was shown it by some
+        dutch friends while traveling and decided it would be the perfect
+        project to explore using web sockets with Next JS. Check out the actual{" "}
         <Link href="https://www.bernhardweber.de/punto.html">
           <b className="hover:underline">board game</b>
         </Link>{" "}
         by Bernhard Weber!
       </p>
+      <br></br>
+      <p>Play a local four player game:</p>
       <Link href="/game">
-        <h2 className="text-2xl font-bold hover:underline">Play Locally</h2>
+        <h2 className="text-xl font-bold hover:underline">Local Game</h2>
       </Link>
-      <Authentication></Authentication>
+      <br></br>
+      <p>Sign in to play a multiplayer version with up to 4 players:</p>
+      <SessionProvider>
+        <LoginOrOut></LoginOrOut>
+        <CreateRoom></CreateRoom>
+        <JoinRoom></JoinRoom>
+      </SessionProvider>
     </main>
+  );
+}
+
+function JoinRoom() {
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [roomCode, setRoomCode] = useState("");
+  if (!session) return null;
+
+  const handleJoin = () => {
+    // redirect to room
+    const path = "/room/" + roomCode;
+    router.push(path);
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        className="text-black"
+        placeholder="Room Code"
+        onChange={(e) => setRoomCode(e.target.value)}
+      ></input>
+      <button onClick={handleJoin}>Join a room</button>
+    </>
+  );
+}
+
+function CreateRoom() {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  if (!session) return null;
+
+  const createRoom = async () => {
+    const res = await fetch("api/room/create");
+    if (!res.ok) {
+      throw new Error("Could not create room");
+    }
+    const body = (await res.json()) as { id: string };
+    console.log(body);
+    // redirect to room
+    const path = "/room/" + body.id;
+    router.push(path);
+  };
+  return (
+    <>
+      <button onClick={createRoom}>Create a room</button>
+    </>
   );
 }
