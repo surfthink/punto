36da36262cec11df7099451884878317be57f128
bb-33 <jonolj56@gmail.com>
2024-01-15 14:00:05 +0800

    remove old files

--- src/app/room/pusher.ts
+++ src/app/room/pusher.ts
@@ -1,7 +1,6 @@
 import Pusher from "pusher-js";
 
 export const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
-  // need to make env var
   cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
   authEndpoint: "/api/pusher/auth", // different from docs? https://pusher.com/docs/channels/server_api/authorizing-users/
 });
