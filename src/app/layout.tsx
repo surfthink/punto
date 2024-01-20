00c50730f2490407e2960c917665efb4feaccc49
bb-33 <jonolj56@gmail.com>
2024-01-20 12:33:42 +0800

    add shadcn components to project and first attempt at navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./_components/nav/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="m-1">
+          <Navbar></Navbar>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
