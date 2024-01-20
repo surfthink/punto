315b0eb45ec567ab2a33e570076d6707ac28c8aa
bb-33 <jonolj56@gmail.com>
2024-01-20 11:06:20 +0800

    move about information to about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">About</h1>
+      <p>
+        An online version of a table top game I was shown by some dutch friends
+        while traveling. I decided it would be the perfect project to explore
+        using web sockets with Next JS. Check out the actual{" "}
+        <Link href="https://www.bernhardweber.de/punto.html">
+          <b className="hover:underline">board game</b>
+        </Link>{" "}
+        by Bernhard Weber!
+      </p>
+      <p>
+        The source code of this project can be found on my{" "}
+        <Link href="https://github.com/BB-33/punto">
+          <b className="hover:underline">Github</b>
+        </Link>{" "}
+      </p>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,6 @@ import { authOptions } from "./api/auth/[...nextauth]/authOptions";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-3xl font-bold underline">Punto Online</h1>
-      <p>
-        An online version of the European table top game. I was shown it by some
-        dutch friends while traveling and decided it would be the perfect
-        project to explore using web sockets with Next JS. Check out the actual{" "}
-        <Link href="https://www.bernhardweber.de/punto.html">
-          <b className="hover:underline">board game</b>
-        </Link>{" "}
-        by Bernhard Weber!
-      </p>
-      <br></br>
       <p>Play a local four player game:</p>
       <Link href="/game">
         <h2 className="text-xl font-bold hover:underline">Local Game</h2>
