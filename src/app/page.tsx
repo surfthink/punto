c0e10b55ef28ac4c118dabf6691f3016075fd9cf
bb-33 <jonolj56@gmail.com>
2024-01-20 15:47:41 +0800

    trying to make home page look better using shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,92 @@
-import Link from "next/link";
-import { LoginOrOut } from "./_components/authenticate/Authentication";
+"use client";
 import { redirect } from "next/navigation";
 import { createRoom } from "./_actions/room";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/authOptions";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Home() {
+  const [gameMode, setGameMode] = useState<"online" | "local">("online");
+  const handleGameModeChange = (value: string) => {
+    setGameMode(value as "online" | "local");
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <p>Play a local four player game:</p>
-      <Link href="/game">
-        <h2 className="text-xl font-bold hover:underline">Local Game</h2>
-      </Link>
-      <br></br>
-      <p>Sign in to play a multiplayer version with up to 4 players:</p>
-      <JoinRoom></JoinRoom>
-      <CreateRoom></CreateRoom>
+    <main className="flex min-h-screen flex-col items-center gap-6">
+      <div className="flex gap-2 text-3xl">
+        {gameMode === "online" && (
+          <>
+            <Button onClick={createRoom}>Create room</Button>
+            <div> or </div>
+            <Button>Play in room</Button>
+            <Input></Input>
+          </>
+        )}
+        {gameMode === "local" && <Button>Play</Button>}
+      </div>
+      <SelectGameMode onValueChange={handleGameModeChange}></SelectGameMode>
     </main>
   );
 }
 
-async function JoinRoom() {
-  const session = await getServerSession(authOptions);
-  if (!session) return null;
+// async function JoinRoom() {
+//   const session = await getServerSession(authOptions);
+//   if (!session) return null;
 
-  async function joinRoom(formData: FormData) {
-    "use server";
-    redirect("/room/" + formData.get("roomId"));
-  }
+//   async function joinRoom(formData: FormData) {
+//     "use server";
+//     redirect("/room/" + formData.get("roomId"));
+//   }
 
-  return (
-    <form action={joinRoom}>
-      <input
-        type="text"
-        name="roomId"
-        className="text-black"
-        placeholder="Room Code"
-      ></input>
-      <button type="submit">Join a room</button>
-    </form>
-  );
+//   return (
+//     <form action={joinRoom}>
+//       <input
+//         type="text"
+//         name="roomId"
+//         className="text-black"
+//         placeholder="Room Code"
+//       ></input>
+//       <button type="submit">Join a room</button>
+//     </form>
+//   );
+// }
+
+// async function CreateRoom() {
+//   const session = await getServerSession(authOptions);
+//   if (!session) return null;
+//   return (
+//     <form action={createRoom}>
+//       <button type="submit">Create a room</button>
+//     </form>
+//   );
+// }
+
+interface SelectGameModeProps {
+  onValueChange?: (value: string) => void;
 }
-
-async function CreateRoom() {
-  const session = await getServerSession(authOptions);
-  if (!session) return null;
+function SelectGameMode({ onValueChange }: SelectGameModeProps) {
   return (
-    <form action={createRoom}>
-      <button type="submit">Create a room</button>
-    </form>
+    <div className="flex items-center gap-3">
+      <div>Gamemode: </div>
+      <Select defaultValue="online" onValueChange={onValueChange}>
+        <SelectTrigger className="w-[280px]">
+          <SelectValue placeholder="Online"></SelectValue>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="online">Online</SelectItem>
+          <SelectItem value="local">Local</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
   );
 }
