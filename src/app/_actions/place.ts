cc3ce5b60dff115ae1b482a87f038fab9196c106
bb-33 <jonolj56@gmail.com>
2024-01-22 21:00:18 +0800

    remove game finished state (debugging deployment)

--- src/app/_actions/place.ts
+++ src/app/_actions/place.ts
@@ -54,7 +54,7 @@ export async function place(card: PlacedCard, roomId: string) {
   const winner = await checkForWin(roomId, card.x, card.y, card.c);
   if (winner) {
     console.log("game over!");
-    await endGame(roomId);
+    // await endGame(roomId);
     broadcastToRoom(roomId, {
       action: "GAME_OVER",
       data: { winner: { username: await getUsernameCookie() } },
