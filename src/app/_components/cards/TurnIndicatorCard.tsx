fa2d33555890a2fddda1a7c64cf192dfc0b32a30
bb-33 <jonolj56@gmail.com>
2024-02-07 10:49:32 +0800

    added a popover to show usernames of players

--- src/app/_components/cards/TurnIndicatorCard.tsx
+++ src/app/_components/cards/TurnIndicatorCard.tsx
@@ -4,6 +4,12 @@ import { cn } from "@/lib/utils";
 import { AvatarIcon } from "@radix-ui/react-icons";
 import { PlayerInfo } from "@/app/_actions/room";
 
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
 export default function TurnIndicatorCard({
   username,
   turn,
@@ -17,40 +23,52 @@ export default function TurnIndicatorCard({
 }) {
   if (!player) return <Skeleton></Skeleton>;
   return (
-    <div
-      key={player.username}
-      className={cn(
-        "border border-white  border-8 rounded-lg z-10 w-full",
-        player.color === Color.RED && turn === player.username
-          ? "border-red-400"
-          : "",
-        player.color === Color.GREEN && turn === player.username
-          ? "border-green-400"
-          : "",
-        player.color === Color.BLUE && turn === player.username
-          ? "border-blue-400"
-          : "",
-        player.color === Color.YELLOW && turn === player.username
-          ? "border-yellow-400"
-          : "",
-        index === 0 ? "col-start-1 row-start-1" : "",
-        index === 1 ? "col-start-2 row-start-1" : "",
-        index === 2 ? "col-start-1 row-start-2" : "",
-        index === 3 ? "col-start-2 row-start-2" : ""
-      )}
-    >
-      <div className=" bg-black flex items-center justify-center p-1">
-        <AvatarIcon
-          width={40}
-          height={40}
+    <Popover>
+      <PopoverTrigger asChild>
+        <div
+          key={player.username}
           className={cn(
-            player.color === Color.RED ? "text-red-400" : "",
-            player.color === Color.GREEN ? "text-green-400" : "",
-            player.color === Color.BLUE ? "text-blue-400" : "",
-            player.color === Color.YELLOW ? "text-yellow-400" : ""
+            "border border-white  border-8 rounded-lg z-10 w-full",
+            player.color === Color.RED && turn === player.username
+              ? "border-red-400"
+              : "",
+            player.color === Color.GREEN && turn === player.username
+              ? "border-green-400"
+              : "",
+            player.color === Color.BLUE && turn === player.username
+              ? "border-blue-400"
+              : "",
+            player.color === Color.YELLOW && turn === player.username
+              ? "border-yellow-400"
+              : "",
+            index === 0 ? "col-start-1 row-start-1" : "",
+            index === 1 ? "col-start-2 row-start-1" : "",
+            index === 2 ? "col-start-1 row-start-2" : "",
+            index === 3 ? "col-start-2 row-start-2" : ""
           )}
-        ></AvatarIcon>
-      </div>
-    </div>
+        >
+          <div
+            className={cn(
+              "bg-black hover:bg-gray-800 rounded-lg flex items-center justify-center p-1",
+              turn === player.username ? "rounded-none" : ""
+            )}
+          >
+            <AvatarIcon
+              width={40}
+              height={40}
+              className={cn(
+                player.color === Color.RED ? "text-red-400" : "",
+                player.color === Color.GREEN ? "text-green-400" : "",
+                player.color === Color.BLUE ? "text-blue-400" : "",
+                player.color === Color.YELLOW ? "text-yellow-400" : ""
+              )}
+            ></AvatarIcon>
+          </div>
+        </div>
+      </PopoverTrigger>
+      <PopoverContent className="w-fit">
+        <p>{player.username}</p>
+      </PopoverContent>
+    </Popover>
   );
 }
