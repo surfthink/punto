1488f66df81ab870807a83c774b1d333a3d1624f
bb-33 <jonolj56@gmail.com>
2024-02-08 11:35:13 +0800

    fix some environment variable names

--- src/app/api/pusher/pusher.ts
+++ src/app/api/pusher/pusher.ts
@@ -5,9 +5,9 @@ import Pusher from "pusher";
 
 export const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID!,
-  key: process.env.PUSHER_KEY!,
+  key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
   secret: process.env.PUSHER_SECRET!,
-  cluster: process.env.PUSHER_CLUSTER!,
+  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
   useTLS: true,
 });
 
