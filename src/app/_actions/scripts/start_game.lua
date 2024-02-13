543e2d536fd1cc1e77df08c76b12c694c4c519c8
bb-33 <jonolj56@gmail.com>
2024-02-13 13:35:10 +0800

    add basic game info to the script

--- src/app/_actions/scripts/start_game.lua
+++ src/app/_actions/scripts/start_game.lua
@@ -14,16 +14,14 @@ local stateObjectKey = KEYS[3] .. roomId
 local roomState = redis.call("HGET", stateObjectKey, "state")
 if roomState == "WAITING" then
     redis.call("HSET", stateObjectKey, "state", "PLAYING")
-    
+    redis.call("LPUSH", orderListKey, players)
+    redis.call("SADD", playerSetKey, players)
+    redis.call("HSET", stateObjectKey, "turn", 0)
     return "inside"
 end
 return "outside"
 
 
--- redis.call("LPUSH", orderListKey, players)
--- redis.call("SADD", playerSetKey, players)
--- redis.call("HSET", stateObjectKey, "state", "PLAYING")
--- redis.call("HSET", stateObjectKey, "turn", 0)
 
 
 
