949043f1489b7f9dc398d47c1ce6118fe1920f72
bb-33 <jonolj56@gmail.com>
2024-02-13 13:27:46 +0800

    configure webpack to read in the lua script as a string

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
+const { config } = require('process')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack:(config,{ buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+        config.module.rules.push({
+            test: /\.lua$/,
+            type: 'asset/source'
+        })
+        return config
+    }
+}
 
 module.exports = nextConfig
